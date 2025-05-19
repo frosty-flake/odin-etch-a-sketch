8886c75bbd0b8dacfd61ee408b76babd0ff55485
Victor <johnjohnjohnnyjoe@gmail.com>
2025-05-19 12:35:38 +0800

    Add resize button
    Added a resize button that allows changing the size.
    
    However, the button breaks on pressing cancel. To fix.

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const container = document.querySelector("#container");
 
 function makeGrid(dimension) {
+    while (container.firstElementChild) {
+        container.removeChild(container.firstElementChild);
+    };
+
    for (let i = 1; i <= dimension; i++) {
     let rowDiv = document.createElement("div");
     rowDiv.classList.add("sketch-row");
@@ -11,14 +15,33 @@ function makeGrid(dimension) {
     }
     container.appendChild(rowDiv);
     }; 
+
+    const boxes = document.querySelectorAll(".sketch-box");
+
+    boxes.forEach((box) => {
+        box.addEventListener("mouseenter", (e) => {
+            box.classList.add("hovered");
+        });
+    });
 };
 
 makeGrid(64);
 
-const boxes = document.querySelectorAll(".sketch-box");
+const sizeButton = document.querySelector("#size-button");
 
-boxes.forEach((box) => {
-    box.addEventListener("mouseenter", (e) => {
-        box.classList.add("hovered");
-    });
+sizeButton.addEventListener("click", () => {
+    let size = -1;
+
+    while (size < 0 || size > 100){
+        size = prompt("Enter your grid size:",50);
+        if (size < 0) {
+            alert("That's less than zero!");
+        } else if (size > 100) {
+            alert("That's too big!");
+        };
+
+    }
+    if (size != -1) {
+        makeGrid(size);
+    };
 });
