e43f427cde9b15d8a93b16710312f94b677c939e
Victor <johnjohnjohnnyjoe@gmail.com>
2025-05-19 12:11:38 +0800

    Add script to shade box gray when hovered

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 const container = document.querySelector("#container");
 
-for (let i = 1; i <= 16; i++) {
+function makeGrid(dimension) {
+   for (let i = 1; i <= dimension; i++) {
     let rowDiv = document.createElement("div");
     rowDiv.classList.add("sketch-row");
-    for (let j = 1; j <= 16; j++) {
+    for (let j = 1; j <= dimension; j++) {
         let boxDiv = document.createElement("div");
         boxDiv.classList.add("sketch-box");
         rowDiv.appendChild(boxDiv);
     }
     container.appendChild(rowDiv);
+    }; 
 };
+
+makeGrid(64);
+
+const boxes = document.querySelectorAll(".sketch-box");
+
+boxes.forEach((box) => {
+    box.addEventListener("mouseenter", (e) => {
+        box.classList.add("hovered");
+    });
+});
